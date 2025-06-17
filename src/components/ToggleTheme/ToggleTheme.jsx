555cc28e3981ce29570bf89689ebb815c9a9fffd
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-06-17 15:47:53 -0300

    feat: adding dark mode

---
+++ src/components/ToggleTheme/ToggleTheme.jsx
@@ -0,0 +1,22 @@
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid"
+import { useEffect } from "react"
+
+const ToggleTheme = () => {
+    const systemPreference = window.matchMedia("(prefers-color-scheme: dark)").matches
+    const pageClasses = document.documentElement.classList
+
+    const toggle = () => {
+        document.documentElement.classList.toggle('dark')
+    }
+    
+    useEffect(() => {
+        systemPreference && pageClasses.add("dark")
+    })
+
+    return <div className="hidden sm:block">
+        <MoonIcon className="h-8 text-gray-100 cursor-pointer block dark:hidden" onClick={toggle} />
+        <SunIcon className="h-8 text-gray-100 cursor-pointer hidden  dark:block" onClick={toggle} />
+    </div>
+}
+
+export default ToggleTheme
