aa4fcca579c5ab79d27ff9c5c31fd73a30957d86
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-06-18 15:55:29 -0300

    feat: adding both hover and animation effects to form

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,5 +1,5 @@
 const Input = props => {
-    return <input className="dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full py-1 pl-5 outline-none w-full max-w-lg" {...props} />
+    return <input className="bg-gray-200 dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full py-3 pl-5 outline-none w-full max-w-lg" {...props} />
 }
 
 export default Input
