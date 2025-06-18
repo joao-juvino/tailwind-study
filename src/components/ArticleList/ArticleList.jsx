f283c42eb8db08f5e0bf1c13539cc1e178af72d2
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-06-18 14:11:56 -0300

    feat: styling Article components in taildwind

--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -1,9 +1,10 @@
 import data from "../../../articles.json"
+import Article from "../Article/Artile"
 
 const ArticleList = () => {
-    return <div className="mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3">
+    return <div className="mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3 lg:max-w-[1000px] xl:max-w-[1300px]">
         {
-            data.map(props => <p>{props.title}</p>)
+            data.map((props, index) => <Article key={index} {...props} />)
         }
     </div>
 }
