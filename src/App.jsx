fa44ae49524b9af4279b412c9379b0c78509462f
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-06-18 15:26:19 -0300

    feat: adding form to get user info

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import ArticleList from "./components/ArticleList/ArticleList";
 import Header from "./components/Header/Header";
+import Form from "./components/Form/Form";
 
 const App = () => {
+  const [user, setUser] = useState()
+  const hasUser = Boolean(user)
   return <>
-    <Header />
-    <ArticleList />
+    <Header user={user} />
+    {hasUser && <ArticleList />}
+    {hasUser || <Form onSubmit={setUser} />}
   </>
 }
 
