9d0084b38651495e4a26f79686e1c517b89c1a60
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-06-18 14:45:13 -0300

    feat: adding field for tags in Article component

--- src/components/Article/Artile.jsx
+++ src/components/Article/Artile.jsx
@@ -1,6 +1,11 @@
 const Article = ({ title, text, tags, image, alt }) => {
-    return <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl shadow-lg flex flex-col items-center">
+    return <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl shadow-lg flex flex-col items-center gap-2">
         <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">{title}</h3>
+
+        <div className="w-full justify-end gap-2 pr-5 hidden sm:flex">
+            {tags.map(tag => <span key={tag} className="bg-alura-100 dark:bg-dark-100 px-2 py-1 rounded-full text-gray-200 font-bold text-xs uppercase">{tag}</span>)}
+        </div>
+
         <div className="grid gap-5">
             {
                 text.map((content, index) => <p key={index} className="text-alura-200 dark:text-gray-400">{content}</p>)
